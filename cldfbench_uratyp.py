73e9b1f06548016d2f3dc9fb8d2601fdc53491ad
Robert Forkel <xrotwang@googlemail.com>
2021-06-03 12:54:55 +0200

    only include valid values

--- cldfbench_uratyp.py
+++ cldfbench_uratyp.py
@@ -16,6 +16,7 @@ class Dataset(BaseDataset):
     def cmd_makecldf(self, args):
         args.writer.cldf.add_component('ParameterTable', 'Area')
         args.writer.cldf.add_component('LanguageTable')
+        args.writer.cldf.add_component('CodeTable')
         args.writer.cldf['LanguageTable', 'Glottocode'].null = ['?']
         args.writer.cldf['LanguageTable', 'ISO639P3code'].null = ['?']
 
@@ -27,14 +28,26 @@ class Dataset(BaseDataset):
 
         for param in self.raw_dir.read_csv('Features.csv', dicts=True):
             args.writer.objects['ParameterTable'].append(param)
+            for code, name in [('1', 'yes'), ('0', 'no')]:
+                args.writer.objects['CodeTable'].append(dict(
+                    ID='{}-{}'.format(param['ID'], code),
+                    Name=code,
+                    Description=name,
+                    Parameter_ID=param['ID'],
+                ))
 
         for row in self.raw_dir.read_csv('Finaldata.csv', dicts=True):
             for k in row:
                 lid = lmap[row['language']]
                 if k.startswith('UT'):
+                    if row[k] in ['', 'N/A']:  # don't even include the rows
+                        continue
+                    if '?' in row[k]:
+                        continue
                     args.writer.objects['ValueTable'].append(dict(
                         ID='{}-{}'.format(lid, k),
                         Language_ID=lid,
                         Parameter_ID=k,
                         Value=row[k],
+                        Code_ID='{}-{}'.format(k, row[k]),
                     ))
