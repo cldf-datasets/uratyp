cc968f96da3756ab16f499f6d6707cf14d214982
Robert Forkel <xrotwang@googlemail.com>
2022-01-28 12:55:27 +0100

    next round of examples

--- uratypcommands/examples.py
+++ uratypcommands/examples.py
@@ -1,19 +1,24 @@
 """
 
 """
+import re
+
 from csvw.dsv import reader, UnicodeWriter
 from clldutils.clilib import PathType
 from cldfbench_uratyp import Dataset
 
+WORD_PATTERN = re.compile("(?P<Primary_Text>[^[]+)\[(?P<IPA>[^]]+)]\s*('|’|‘)(?P<Translation>[^'’’]+)('|’|’)")
+GLOSS_PATTERN = re.compile("(?P<Primary_Text>[^[]+)\[(?P<Analyzed>[^]]+)]\s*(?P<Gloss>[a-zA-Z.123]+)")
+
 
 def register(parser):
     parser.add_argument('lang')
+    parser.add_argument('action', default='split')
 
 
 def parse(row):
     c = row['Example'].count(';')
     if c in [2, 3]:
-        print('hihi')
         if c == 2:
             pt, gloss, translation = row['Example'].split(';')
             comment = ''
@@ -23,7 +28,17 @@ def parse(row):
         parsed['Analyzed'] = '\t'.join(parsed['Primary_Text'].split())
         yield [parsed.get(k, row[k]) for k in row]
     else:
-        yield [row[k] for k in row]
+        m = WORD_PATTERN.fullmatch(row['Example'])
+        if m:
+            parsed = m.groupdict()
+            yield [parsed.get(k, row[k]) for k in row]
+        else:
+            m = GLOSS_PATTERN.fullmatch(row['Example'])
+            if m:
+                parsed = m.groupdict()
+                yield [parsed.get(k, row[k]) for k in row]
+            else:
+                yield [row[k] for k in row]
 
 
 def split(row):
@@ -39,7 +54,7 @@ def run(args):
             with UnicodeWriter(p) as w:
                 w.writerow(rows[0].keys())
                 for row in rows:
-                    w.writerows(list(parse(row)))
+                    w.writerows(list((split if args.action == 'split' else parse)(row)))
 
         #    w.writerow(['ID', 'Example', 'Primary_Text', 'IPA', 'Analyzed', 'Gloss', 'Translation', 'Comment'])
         #    for row in rows:
