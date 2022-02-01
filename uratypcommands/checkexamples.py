500c879d66a817ad81d66ed7da1828d59bb393da
Robert Forkel <xrotwang@googlemail.com>
2022-02-01 09:26:42 +0100

    another one

--- uratypcommands/checkexamples.py
+++ uratypcommands/checkexamples.py
@@ -1,19 +1,32 @@
 """
 
 """
+import collections
+
 from csvw.dsv import reader
 from cldfbench_uratyp import Dataset
 
 
 def run(args):
+    inv = 0
+    i = collections.Counter()
     for p in Dataset().raw_dir.joinpath('UT', 'language-tables').glob('*_examples.csv'):
         for row in reader(p, dicts=True):
             if not row['Primary_Text']:
                 print(p.stem, row['ID'], row['Example'])
+                inv += 1
+                continue
             if row['Analyzed'] and row['Gloss']:
                 a = row['Analyzed'].strip().split()
                 g = row['Gloss'].strip().split()
                 if len(a) != len(g):
+                    inv += 1
                     print(p.stem, row['ID'])
                     print(a)
                     print(g)
+                    continue
+            i.update([p.stem])
+    for k, v in i.most_common():
+        print(k, v)
+    print(sum(i.values()))
+    print(inv)
